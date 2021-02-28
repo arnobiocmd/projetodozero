8915dd7e2c565b82005e45a6dab8165a9d98df33
arnobio paulino fernandes <arnobiopaulinofernandes@gmail.com>
2021-02-28 19:35:36 -0300

    primeiro commit do projeto foram feita as marcacoes com HTML apenas

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto 01</title>
+	  <meta charset="UTF-8">
+	  <meta name="description" content="Site de teste">
+	  <meta name="keywords" content="palavras,chaves">
+	  <meta name="author" content="Arnobio">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+	<header>
+		<div class="center">
+			<div class="logo"><a href="">LOGOMARCA</a></div>
+			<nav class="menu-desktop">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+			<nav class="menu-mobile">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="banner-principal">
+		<div class="center">
+			<form>
+				<h2>Qual seu melhor e-mail?</h2>
+				<input type="email" name="email">
+				<input type="submit" name="acao" value="Cadastrar!">
+			</form>
+		</div>
+	</section>
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="w50">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="w50">
+				<img src="assets/img/imgava.jpg">
+			</div>
+		</div>
+	</section>
+	<section class="especialidas">
+		<div class="center">
+			<h2 class="title">Especialidades</h2>
+			<div class="box-especialidades">
+				<h3>Icone1</h3>
+				<h3>CSS</h3>
+				<p>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+			</div>
+			<div class="box-especialidades">
+				<h3>Icone2</h3>
+				<h3>HTML</h3>
+				<p>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+			</div>
+			<div class="box-especialidades">
+				<h3>Icone3</h3>
+				<h3>JAVASCRIPT</h3>
+				<p>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+			</div>
+		</div>
+	</section>
+	<section class="extras">
+		<div class="center">
+			<div class="w50">
+			<h2 class="title">Depoimentos</h2>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+					</p>
+					<p class="nome-autor">Lorem ipson</p>
+				</div>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+					</p>
+					<p class="nome-autor">Lorem ipson</p>
+				</div>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+					</p>
+					<p class="nome-autor">Lorem ipson</p>
+				</div>
+			</div>
+			<div class="w50">
+			<h2 class="title">Serviços</h2>
+				<div class="servicos">
+					<ul>
+						<li>ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+						consequat. Duis aute irure dolor in reprehenderit in voluptate 
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+						<li>ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+						consequat. Duis aute irure dolor in reprehenderit in voluptate 
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+						<li>ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+						consequat. Duis aute irure dolor in reprehenderit in voluptate 
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="center">
+			<p>Todos os direitos resevados</p>
+		</div>
+	</footer>
+</body>
+</html>
